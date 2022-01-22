daf817c156c9d736f76b6a61005612bd8eea040f
Bidooooooooooooo <ana_bedo>
2022-01-22 23:01:13 +0200

    Rx

--- app/src/main/java/com/example/postsnow/data/PostInteface.java
+++ app/src/main/java/com/example/postsnow/data/PostInteface.java
@@ -4,11 +4,13 @@ import com.example.postsnow.pojo.PostsModel;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PostInteface {
 
     @GET("posts")
-    Call<List<PostsModel>>getPosts();
+    Observable<List<PostsModel>> getPosts();
 }
--- app/src/main/java/com/example/postsnow/ui/main/POstViweModel.java
+++ app/src/main/java/com/example/postsnow/ui/main/POstViweModel.java
@@ -1,5 +1,8 @@
 package com.example.postsnow.ui.main;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -8,36 +11,64 @@ import com.example.postsnow.pojo.PostsModel;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class POstViweModel extends ViewModel {
-         MutableLiveData<List<PostsModel>> mutableLiveData=new MutableLiveData<>();
-         MutableLiveData<String> posts = new MutableLiveData<>();
+    MutableLiveData<List<PostsModel>> mutableLiveData = new MutableLiveData<>();
+    MutableLiveData<String> posts = new MutableLiveData<>();
+    private String TAG;
 
 
-
-    public void getPosts(){
-        Retrofit retrofit=new Retrofit.Builder()
+    public void getPosts() {
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
-        PostInteface postInteface=retrofit.create(PostInteface.class);
-        Call<List<PostsModel>> call=postInteface.getPosts();
-        call.enqueue(new Callback<List<PostsModel>>() {
+
+        PostInteface postInteface = retrofit.create(PostInteface.class);
+        Observable<List<PostsModel>> observable = postInteface.getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+        Observer<List<PostsModel>> observer=new Observer<List<PostsModel>>() {
             @Override
-            public void onResponse(Call<List<PostsModel>> call, Response<List<PostsModel>> response) {
-                mutableLiveData.setValue(response.body());
+            public void onSubscribe(@NonNull Disposable d) {
+
             }
 
             @Override
-            public void onFailure(Call<List<PostsModel>> call, Throwable t) {
-                posts.setValue("Error");
+            public void onNext(@NonNull List<PostsModel> postsModels) {
+            mutableLiveData.setValue(postsModels);
             }
-        });
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        observable.subscribe(observer);
+
+
+
     }
 }
+
+
